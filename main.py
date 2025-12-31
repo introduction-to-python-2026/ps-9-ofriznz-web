3677e5815e05786fea0629a18c7742ba7c509c48
ofriznz-web <ofriznz@gmail.com>
2025-12-31 22:02:22 +0200

    Update main.py 1

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
-from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.pipeline import Pipeline
 import pandas as pd
+
+df = pd.read_csv('parkinsons.csv')
+df = df.dropna()
+df.head()
+
+features = ['spread1',  'PPE']
+X = df[features]
+y = df['status']
+
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler()
+X = scaler.fit_transform(X)
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=20)
+
+from sklearn.svm import SVC
+
+model = SVC()
+model.fit(X_train, y_train)
+
+from sklearn.metrics import accuracy_score
+
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy}')
+
 import joblib
 
-input_features = ['spread1', 'PPE']
-output_feature = 'status'
-
-X = df[input_features]
-y = df[output_feature]
-
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=15)
-
-model_pipeline = Pipeline([
-    ('scaler', MinMaxScaler()),
-    ('classifier', LogisticRegression(random_state=15))
-])
-
-model_pipeline.fit(X_train, y_train)
-
-y_pred = model_pipeline.predict(X_val)
-accuracy = accuracy_score(y_val, y_pred)
-print(f"Model Accuracy: {accuracy:.4f}")
-
-joblib.dump(model_pipeline, 'model.joblib') 
-print("Model saved successfully!")
+joblib.dump(model, 'Parkinsons.joblib')
